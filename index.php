3216f6af50fc070434160744a9de5decb32866d6
Josef Kufner <jk@frozen-doe.net>
2011-09-25 12:51:09 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Copyright (c) 2010, Josef Kufner  <jk@frozen-doe.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Yes, you can freely skip this file if you set your web server
+ * directly to core/index.php.
+ */
+
+
+/* Define directory and file names.
+ * Each DIR_* must be slash-terminated!
+ * These constanst will be defined (but not overwritten) in core/index.php.
+ */
+//define('DIR_ROOT',		dirname(dirname(__FILE__)).'/');
+//define('DIR_CORE',		DIR_ROOT.'core/');
+//define('DIR_APP',		DIR_ROOT.'app/');
+//define('DIR_PLUGIN',		DIR_ROOT.'plugin/');
+
+/* bootstrap core */
+require(defined('DIR_CORE') ? DIR_CORE.'index.php' : './core/index.php');
+
